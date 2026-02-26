54d9cfd468ff5b63a9cbe3940b5da5530af8c967
katherine <katherine.malleck@stu.ocsb.ca>
2026-02-26 12:53:46 -0500

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Katherine
+ * Created on: Feb 2026
+ * This program will control the LED.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+// turn on LED
+input.onButtonPressed(Button.A, function () {
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turn off LED
+input.onButtonPressed(Button.B, function () {
+  basic.showIcon(IconNames.No)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+})
