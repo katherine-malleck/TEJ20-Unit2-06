41adc35ecb93fea48184fa9a3e0d41f57bd50e20
katherine <katherine.malleck@stu.ocsb.ca>
2026-02-26 18:01:51 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,20 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Katherine
+Created on: Feb 2026
+Turns an LED on a breadboard on and off.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+
+# setup
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
